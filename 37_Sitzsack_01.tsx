dae5e3b6e538167fff42df94c9b931b70b41ba55
EvaSeidlmayer <seidlamyer@zbmed.de>
2020-12-19 20:53:18 +0100

    added my test files

---
+++ 37_Sitzsack_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="37_Sitzsack_01" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="tiles/37_Sitzsack_01.png" width="32" height="32"/>
+</tileset>
