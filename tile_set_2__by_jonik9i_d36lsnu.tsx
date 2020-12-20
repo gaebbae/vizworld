d1827cf1efaed78e40095223d9c181faa65dff5d
gaebbae <75264895+gaebbae@users.noreply.github.com>
2020-12-20 13:23:40 +0100

    Delete tile_set_2__by_jonik9i_d36lsnu.tsx

--- tile_set_2__by_jonik9i_d36lsnu.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="tile_set_2__by_jonik9i_d36lsnu" tilewidth="32" tileheight="32" tilecount="1274" columns="49">
- <image source="tiles/tile_set_2__by_jonik9i_d36lsnu.jpg" width="1589" height="833"/>
-</tileset>
