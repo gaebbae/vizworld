243a94c33e8afd9f917a6504568849c3e3939cc1
gaebbae <75264895+gaebbae@users.noreply.github.com>
2021-04-13 17:43:52 +0200

    Add files via upload

---
+++ tiles/catspritesx4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.11.25" name="catspritesx4" tilewidth="32" tileheight="32" tilecount="102" columns="17">
+ <image source="tiles/catspritesx4.gif" trans="003039" width="548" height="200"/>
+</tileset>
---
+++ tiles/kadse.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.11.25" name="kadse" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="tiles/kadse.png" trans="003039" width="88" height="64"/>
+</tileset>
